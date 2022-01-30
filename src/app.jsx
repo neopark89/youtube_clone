f0b301d39f27181a15f69ba898572a36388ae78c
neopark <neopark89@gmail.com>
2022-01-30 22:38:33 +0900

    show video list with title

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './app.css';
+import VideoList from './components/video_list/video_list';
 
 function App() {
+
+const [videos, setVideos] = useState([]);
+
+useEffect(() => {
+  const requestOptions = {
+    method: 'GET',
+    redirect: 'follow'
+  };
+  
+  fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAauY_bJYhEmNLQhRKda62jTmSj8siRuv8", requestOptions)
+    .then(response => response.json())
+    .then(result => setVideos(result.items))
+    .catch(error => console.log('error', error));
+}, [])
+
   return (
-    <h1>hello:)</h1>
-  );
+    <VideoList videos={videos} />
+  )
 }
 
 export default App;
---
+++ src/components/video_item/video_item.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const VideoItem = (props) => <hi>{props.video.snippet.title}</hi>;
+
+export default VideoItem;
