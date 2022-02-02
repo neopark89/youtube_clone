6dec03609cf7777540523a8a28052037a190cd09
neopark <neopark89@gmail.com>
2022-02-02 17:31:49 +0900

    search making

--- src/app.jsx
+++ src/app.jsx
@@ -1,26 +1,49 @@
-import React, { useState, useEffect } from 'react';
-import './app.css';
-import VideoList from './components/video_list/video_list';
+import React, { useState, useEffect } from "react";
+import styles from "./app.module.css";
+import Header from "./components/header/search_header";
+import VideoList from "./components/video_list/video_list";
 
 function App() {
+  const [videos, setVideos] = useState([]);
 
-const [videos, setVideos] = useState([]);
+  const search = (query) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
 
-useEffect(() => {
-  const requestOptions = {
-    method: 'GET',
-    redirect: 'follow'
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyAauY_bJYhEmNLQhRKda62jTmSj8siRuv8`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) =>
+        result.items.map((item) => ({ ...item, id: item.id.videoId })))
+      .then((items) => setVideos(items))
+      .catch((error) => console.log("error", error));
   };
-  
-  fetch("https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAauY_bJYhEmNLQhRKda62jTmSj8siRuv8", requestOptions)
-    .then(response => response.json())
-    .then(result => setVideos(result.items))
-    .catch(error => console.log('error', error));
-}, [])
+
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAauY_bJYhEmNLQhRKda62jTmSj8siRuv8",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
 
   return (
-    <VideoList videos={videos} />
-  )
+    <div className={styles.app}>
+      <Header onSearch={search} />
+      <VideoList videos={videos} />
+    </div>
+  );
 }
 
 export default App;
