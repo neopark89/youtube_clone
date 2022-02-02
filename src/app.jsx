936b881a9e2c20721ba0db2798539efa1273db69
neopark <neopark89@gmail.com>
2022-02-02 18:15:12 +0900

    add youtube service

--- src/app.jsx
+++ src/app.jsx
@@ -3,39 +3,19 @@ import styles from "./app.module.css";
 import Header from "./components/header/search_header";
 import VideoList from "./components/video_list/video_list";
 
-function App() {
+function App({youtube}) {
   const [videos, setVideos] = useState([]);
 
   const search = (query) => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${query}&type=video&key=AIzaSyAauY_bJYhEmNLQhRKda62jTmSj8siRuv8`,
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) =>
-        result.items.map((item) => ({ ...item, id: item.id.videoId })))
-      .then((items) => setVideos(items))
-      .catch((error) => console.log("error", error));
+    youtube
+    .search(query) // 
+    .then((videos) => setVideos(videos));
   };
 
   useEffect(() => {
-    const requestOptions = {
-      method: "GET",
-      redirect: "follow",
-    };
-
-    fetch(
-      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyAauY_bJYhEmNLQhRKda62jTmSj8siRuv8",
-      requestOptions
-    )
-      .then((response) => response.json())
-      .then((result) => setVideos(result.items))
-      .catch((error) => console.log("error", error));
+    youtube
+    .mostPopular() // 
+    .then((videos) => setVideos(videos));
   }, []);
 
   return (
