546262e22ec35215ef9a95419be6af898d0f16af
neopark <neopark89@gmail.com>
2022-02-02 22:49:45 +0900

    back to the list when searching

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,50 @@
 import React, { useState, useEffect } from "react";
 import styles from "./app.module.css";
 import Header from "./components/header/search_header";
+import VideoDetail from "./components/video_detail/video_detail";
 import VideoList from "./components/video_list/video_list";
 
-function App({youtube}) {
+function App({ youtube }) {
   const [videos, setVideos] = useState([]);
+
+  const [selectedVideo, setSelectedVideo] = useState(null);
+
+  const selectVideo = (video) => {
+    setSelectedVideo(video);
+  };
 
   const search = (query) => {
     youtube
-    .search(query) // 
-    .then((videos) => setVideos(videos));
+      .search(query) //
+      .then((videos) => {
+        setVideos(videos);
+        setSelectedVideo(null);
+      });
   };
 
   useEffect(() => {
     youtube
-    .mostPopular() // 
-    .then((videos) => setVideos(videos));
+      .mostPopular() //
+      .then((videos) => setVideos(videos));
   }, []);
 
   return (
     <div className={styles.app}>
       <Header onSearch={search} />
-      <VideoList videos={videos} />
+      <section className={styles.content}>
+        {selectedVideo && (
+          <div className={styles.detail}>
+            <VideoDetail video={selectedVideo} />
+          </div>
+        )}
+        <div className={styles.list}>
+          <VideoList
+            videos={videos}
+            onVideoClick={selectVideo}
+            display={selectedVideo ? "list" : "grid"}
+          />
+        </div>
+      </section>
     </div>
   );
 }
